37141ab02d5d1215715d50f664e0026ef480e3b3
Somade Daniel <Flexsidee@gmail.com>
2021-06-05 17:46:02 +0100

    converted the Navbar from a class to a stateless function and also made use of object destructoring in the Counters Component

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,12 +3,14 @@ import Counter from './counter';
 
 class Counters extends Component {
     render() { 
+        const {onReset, onIncrement, onDelete, counters} = this.props;
+
         return ( 
             <div>
-                <button onClick={this.props.onReset} className="btn btn-primary m-2">Reset</button>
+                <button onClick={onReset} className="btn btn-primary m-2">Reset</button>
                 {this.props.counters.map(counter => <Counter key={counter.id} 
-                                                            onDelete={this.props.onDelete} 
-                                                            onIncrement={this.props.onIncrement} 
+                                                            onDelete={onDelete} 
+                                                            onIncrement={onIncrement} 
                                                             counter={counter} />)}
             </div>
         );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
-    state = {  }
-    render() { 
-        return (  
-            <nav className="navbar navbar-light bg-light">
-                <a href="#" className="navbar-brand">
-                    Navbar <span className="badge badge-spill badge-secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-        );
-    }
+const Navbar = ({totalCounters}) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a href="#" className="navbar-brand">
+                Navbar <span className="badge badge-spill badge-secondary">{totalCounters}</span>
+            </a>
+        </nav> 
+    );
 }
  
 export default Navbar;
