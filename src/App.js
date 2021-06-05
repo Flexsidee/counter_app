7bea49ce1e4dd70703ae6ff5fc4546eaf6e7f769
Somade Daniel <Flexsidee@gmail.com>
2021-06-05 15:47:47 +0100

    added the navbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Counters from './components/counters';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <main className='container'>
+        <Counters />
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <nav className="navbar navbar-light bg-light">
+                <a href="#" className="navbar-brand">
+                    Navbar
+                </a>
+            </nav>
+        );
+    }
+}
+ 
+export default Navbar;
